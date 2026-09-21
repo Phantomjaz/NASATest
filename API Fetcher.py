4e42df406eb186bb3255ec9c291696775baa9b9b
Hemp06 <roblox432sae@outlook.com>
2026-09-21 18:18:46 -0400

    Dataset Change and Working Layer String ID

--- API Fetcher.py
+++ API Fetcher.py
@@ -11,14 +11,17 @@ from PIL import Image
 wms = WebMapService('https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?', version='1.1.1')
 
 # Configure request for MODIS_Terra_CorrectedReflectance_TrueColor
-img = wms.getmap(layers=['AIRS_L3_Carbon_Dioxide_AIRS_AMSU_Monthly'],  # Layers
+img = wms.getmap(layers=['AIRS_L3_Carbon_Monoxide_500hPa_Volume_Mixing_Ratio_Daily_Day'],  # Layers
                  srs='epsg:4326',  # Map projection
-                 bbox=(-180,-90,180,90),  # Bounds
-                 size=(2000, 2000),  # Image size
-                 format='image/jpeg',  # Image format
-                 transparent=True)  # Nodata transparency
+                 bbox=(-180, -90, 180, 90),  # Bounds
+                 size=(1800, 800),  # Image size
+                 format='image/png',  # Image format
+                 transparent=True,
+                 time='2026-09-14')  # Nodata transparency
 
 # Save output PNG to a file
 image_in_memory = Image.open(BytesIO(img.read()))
 
 image_in_memory.show()
+
+print("Finished")
