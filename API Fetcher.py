085abed02d3fc7130fa7b23f92d2b51f48cde00f
Hemp06 <roblox432sae@outlook.com>
2026-09-21 20:50:02 -0400

    Img size & format change

--- API Fetcher.py
+++ API Fetcher.py
@@ -1,15 +1,12 @@
-import cloudinary
-import cloudinary.uploader
-import json
-from datetime import datetime
+from datetime import date, timedelta
 from io import BytesIO
 import matplotlib.pyplot as plt
 from owslib.wms import WebMapService
 from PIL import Image
 import numpy
 
-mapWidth = 1800
-mapHeight = 800
+mapWidth = 720
+mapHeight = 360
 
 min_lon = -180
 max_lon = 180
@@ -19,13 +16,13 @@ max_lat = 90
 wms = WebMapService('https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?', version='1.1.1')
 
 # Configure request for MODIS_Terra_CorrectedReflectance_TrueColor
-img = wms.getmap(layers=['AIRS_L3_Carbon_Monoxide_500hPa_Volume_Mixing_Ratio_Daily_Day'],  # Layers
+img = wms.getmap(layers=['AIRS_L3_Carbon_Monoxide_500hPa_Volume_Mixing_Ratio_Daily_Day'], # Layers
                  srs='epsg:4326',  # Map projection
-                 bbox=(min_lon,min_lat,max_lon,max_lat),  # Bounds
-                 size=(mapWidth,mapHeight),  # Image size
-                 format='image/png',  # Image format
+                 bbox=(min_lon,min_lat,max_lon,max_lat), # Bounds
+                 size=(mapWidth,mapHeight), # Image size
+                 format='image/jpeg', # Image format
                  transparent=True,
-                 time='2026-09-14')  # Nodata transparency
+                 time= date.today() - timedelta(days=7)) # Get data from 7 days before today to get fully processed data
 
 heatImage = Image.open(BytesIO(img.read()))
 
@@ -37,10 +34,7 @@ def getPixelColorAtCoordinates(longitude,latitude):
     coordinates = findPixelCoordinates(longitude,latitude)
     return heatImage.getpixel((coordinates[0],coordinates[1]))
 
-
-# Save output PNG to a file
-
-
+# Show fetched image
 heatImage.show()
 
 print(getPixelColorAtCoordinates(72.877,19.07))
